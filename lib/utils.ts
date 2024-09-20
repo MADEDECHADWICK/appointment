78b7cb84b59b0c0beea704791f8f5bd81c05abd1
MADEDECHADWICK <chadwickmadede@gmail.com>
2024-09-20 11:58:46 +0300

    simplified appointment
    prescription

---
+++ lib/utils.ts
@@ -0,0 +1,40 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
+// FORMAT DATE TIME
+export const formatDateTime = (date: Date, timeZone?: string): { dateTime: string } => {
+  const options: Intl.DateTimeFormatOptions = {
+    // weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+    month: "short", // abbreviated month name (e.g., 'Oct')
+    day: "numeric", // numeric day of the month (e.g., '25')
+    year: "numeric", // numeric year (e.g., '2023')
+    hour: "numeric", // numeric hour (e.g., '8')
+    minute: "numeric", // numeric minute (e.g., '30')
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+  };
+
+  const formattedDateTime: string = new Intl.DateTimeFormat("en-US", {
+    ...options,
+    timeZone: timeZone ?? 'UTC' // Use 'UTC' if no timeZone is provided
+  }).format(date);
+
+  return {
+    dateTime: formattedDateTime,
+  };
+};
+
+export function encryptKey(passkey: string) {
+  return btoa(passkey);
+}
+
+export function decryptKey(passkey: string) {
+  return atob(passkey);
+}
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e0775798e358117a79e036f48b73e15a@o4507922053660672.ingest.de.sentry.io/4507922109235280",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
